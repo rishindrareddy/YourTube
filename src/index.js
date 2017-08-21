6071a2178bf9a6821abe08bb335c479e6c0ed097
rishindrareddy <rishindrareddy@hotmail.com>
2017-08-20 23:49:47 -0700

    components from scratch

--- src/index.js
+++ src/index.js
@@ -1,15 +1,17 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { createStore, applyMiddleware } from 'redux';
 
-import App from './components/app';
-import reducers from './reducers';
+// this is a component, think of it as a class.
+// we are creating a class of a component
+// we create a instance of this component
+// thus created instance is sent to render function.
+// creating instance = <App /> or <App></App>
+const App = function(){
+  return <div>Hola hermano !! </div>;
+}
 
-const createStoreWithMiddleware = applyMiddleware()(createStore);
 
-ReactDOM.render(
-  <Provider store={createStoreWithMiddleware(reducers)}>
-    <App />
-  </Provider>
-  , document.querySelector('.container'));
+// Take this component's generated HTML and put it on the page(in the DOM)
+// first arg = component instance to render
+// second arg = target element into which the react should render this component
+ReactDOM.render(<App />, document.querySelector('.container'));
